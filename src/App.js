0375e40bb819960e74f6c1b7c276a0112eeb6685
Keith Billings <keithbillingsbusiness@gmail.com>
2023-03-18 21:18:23 -0700

    episode 29 - component state

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: 'Keith',
+    };
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
+          <p>My name is {this.state.name}</p>
+          <button>Change Name</button>
         </header>
       </div>
     );
