fd397851eb052cfacc8af03ab71a6865e9314b62
Keith Billings <keithbillingsbusiness@gmail.com>
2023-03-27 23:19:27 -0700

    35 - keys for mapping

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,15 +7,22 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [{ name: 'Linda' }, { name: 'Frank' }, { name: 'Jacky' }, { name: 'Andre' }],
+      monsters: [
+        { name: 'Linda', id: '123' },
+        { name: 'Frank', id: '456' },
+        { name: 'Jacky', id: '789' },
+        { name: 'Andre', id: '321' },
+      ],
     };
   }
 
   render() {
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => (
-          <h1>{monster.name}</h1>
+        {this.state.monsters.map((monster, index) => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
         ))}
       </div>
     );
