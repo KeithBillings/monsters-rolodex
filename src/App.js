ef7b03a72b01747223c0465f6b7eb6bce0b94d33
Keith Billings <keithbillingsbusiness@gmail.com>
2023-04-02 20:59:42 -0700

    44. Monsters Rolodex - Optimizations

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ class App extends Component {
       monsters: [],
       filteredMonsters: [],
     };
-
-    console.log('constructor');
   }
 
   componentDidMount() {
@@ -21,28 +19,26 @@ class App extends Component {
         this.setState(
           () => ({ monsters: users, filteredMonsters: users }),
           () => {
-            console.log(this.state);
+            // console.log(this.state);
           }
         )
       );
-    console.log('com ponentDidMount');
   }
 
+  onSearchChange = (e) => {
+    const input = e.target.value.toLowerCase();
+    const filteredMonsters = this.state.monsters.filter((monster) => monster.name?.toLowerCase().includes(input));
+    this.setState(() => ({ filteredMonsters: filteredMonsters }));
+  };
+
   render() {
-    console.log('render');
+    const { filteredMonsters } = this.state;
+    const { onSearchChange } = this;
+
     return (
       <div className='App'>
-        <input
-          className='search-box'
-          type='search'
-          placeholder='search monsters'
-          onChange={(e) => {
-            const input = e.target.value.toLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => monster.name?.toLowerCase()?.includes(input));
-            this.setState(() => ({ filteredMonsters: filteredMonsters }));
-          }}
-        />
-        {this.state.filteredMonsters.map((monster, index) => (
+        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange} />
+        {filteredMonsters.map((monster, index) => (
           <div key={monster.id}>
             <h1>{monster.name}</h1>
           </div>
