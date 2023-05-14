3b1b04af2c512cf580beded89ccf71c4aa9a2d58
Keith Billings <keithbillingsbusiness@gmail.com>
2023-05-14 15:27:26 -0700

    51. Monsters Rolodex - CardList Component

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -1,16 +1,18 @@
 import { Component } from 'react';
+import CardContainer from '../card/card-container';
+import './card-list.css';
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
 
     return (
-      <div>
-        {monsters.map((monster, index) => (
-          <div key={monster.id}>
-            <h1>{monster.name}</h1>
-          </div>
-        ))}
+      <div className='card-list'>
+        {monsters.map((monster, index) => {
+          const { id, name, email } = monster;
+
+          return <CardContainer key={id} id={id} name={name} email={email} />;
+        })}
       </div>
     );
   }
